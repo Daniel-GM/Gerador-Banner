8550d44c3a23c0d0ac638504bf042899734d4797
Daniel-GM <danielgomesmoura@gmail.com>
2025-01-21 15:50:15 -0300

    inicio do gerador

---
+++ src/App.jsx
@@ -0,0 +1,25 @@
+import { useState } from 'react'
+import './App.css'
+import Banner from './assets/components/Banner'
+
+function App() {
+  
+
+  return (
+    <div className="min-h-screen bg-gray-900 h-full">
+      <div className="container mx-auto p-6 h-full">
+        <div className="grid grid-cols-1 h-full">
+          <div className="space-y-4 bg-gray-800/50 p-6 border-gray-700 border-2 rounded-lg shadow-sm mt-6 grid justify-center items-center">
+            <Banner />
+          </div>
+          <div className="space-y-4 bg-gray-800/50 p-6 border-gray-700 border-2 rounded-lg shadow-sm mt-6">
+            <h1 className="text-2xl text-white">Configurações</h1>
+            {/* config */}
+          </div>
+        </div>
+      </div>
+    </div>
+  )
+}
+
+export default App
---
+++ src/assets/components/Banner.jsx
@@ -0,0 +1,11 @@
+import React from 'react'
+
+const Banner = () => {
+  return (
+    <div className='w-[1280px] h-[333px] bg-white'>
+      
+    </div>
+  )
+}
+
+export default Banner
