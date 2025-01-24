5962ab9807b06bbe07a2736fa9b985046d6fe3a4
Daniel-GM <danielgomesmoura@gmail.com>
2025-01-24 09:56:57 -0300

    limitando a imagem somente a metade da DIV banner

--- src/assets/components/Banner.jsx
+++ src/assets/components/Banner.jsx
@@ -3,17 +3,22 @@ import Title from './Title'
 import Rhombus from './Rhombus'
 
 const Banner = ({ color, options, image }) => {
-  
+
   return (
     <>
       <div
         id="banner"
         className='w-[1280px] h-[333px] relative bg-cover bg-center'
-        style={{ backgroundImage: `linear-gradient(to top, #ffffff80, transparent), ${image ? `url(${image})` : 'url(/mercado.jpg)'}`}}
+        style={{
+          backgroundImage: `linear-gradient(to top, #ffffff80, transparent), ${image ? `url(${image})` : 'url(/mercado.jpg)'}`,
+          backgroundSize: '50% 100%', 
+          backgroundPosition: 'right', 
+          backgroundRepeat: 'no-repeat',
+        }}
       >
         <div
           className='w-[1280px] h-[333px] bg-white'
-          style={{ clipPath: 'polygon(0 0, 55% 0, 92% 100%, 0% 100%)'}}
+          style={{ clipPath: 'polygon(0 0, 55% 0, 92% 100%, 0% 100%)' }}
         ></div>
 
         <div className='grid grid-cols-2 absolute top-0 left-0 w-full h-full'>
